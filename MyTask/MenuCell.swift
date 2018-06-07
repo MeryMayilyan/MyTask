fbe9edd9ea3352d15f02b300be072e594670ab9b
Mery <mery.mayilyan@gmail.com>
2018-06-07 20:36:15 +0400

    Initial commit

---
+++ MyTask/MenuCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuCell.swift
+//  MyTask
+//
+//  Created by Mery  on 6/7/18.
+//  Copyright © 2018 Mery . All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
